6ff554f8580b2f5d68138f2f904985f6df95ae9c
Lariah Christensen <lariah_14@hotmail.com>
2019-07-17 21:56:47 -0600

    added items

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<div>
+			<h1>Diane Event Coordinating</h1>
+		</div>
+
+		<div>
+			<h2>Purpose,Audience, Goal</h2>
+		</div>
+		<ul>
+			Purpose
+			<li>The purpose is to have a home site that advertises business.</li>
+			Audience:
+			<li>Targeted to people who are in need of help planning events, social gatherings, and concerts, etc. </li>
+			Goal:
+			<li>The goal is to have a website that gives insight to client on what Diane does for her business and allows them to see
+			contact info.</li>
+		</ul>
+
+		<div>
+			<h3>Persona</h3>
+		</div>
+		<div>
+			<h4>Profession:</h4>
+			<li>Event Coordinator; Events, social gatherings, rallies, banquets, and concerts</li>
+			<h5>Attitudes & Behaviors:</h5>
+			She is a social butterfly, stays busy, travels alot (event locations), advocate for musicians and artists,
+			creative,
+		</div>
+
+	</body>
+</html>
