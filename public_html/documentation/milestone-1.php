55249f4210e52b17d53dc17236a03869ba7fdc05
Lariah Christensen <lariah_14@hotmail.com>
2019-07-24 16:24:19 -0600

    updated errors, inserted new image

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,47 +5,42 @@
 		<title>Milestone 1</title>
 	</head>
 	<body>
-		<div>
-			<h1>Diane Event Coordinating</h1>
-		</div>
-<img src="IMG_6534[951].JPG" alt="IMG_6534[951]">
-		<div>
-			<h2>Purpose,Audience, Goal</h2>
-		</div>
-		<ul>
-			Purpose
-			<li>The purpose is to have a home site that advertises business.</li>
-			Audience:
-			<li>Targeted to people who are in need of help planning events, social gatherings, and concerts, etc. </li>
-			Goal:
-			<li>The goal is to have a website that gives insight to client on what Diane does for her business and allows them to see
-			contact info.</li>
-		</ul>
+			<h1>Diane's Event Coordinating</h1>
 
-		<div>
-			<h3>Persona</h3>
-		</div>
-		<div>
-			Name: Diane Dubray
-			Age: 42
-			She is a Oglala Lakota Native from the Pine Ridge Reservation, she is the mother of three, and will make your event graceful as well as put the effort in which
-			she gives her family in all that she does.
-		</div>
-		<div>
-			<h4>Profession:</h4>
-			Event Coordinator;
-			<li>Events</li>
-			<li>Social Gatherings</li>
-			<li>Rallies</li>
-			<li>Banquets</li>
-			<li>Concerts</li>
-		</div>
-		<div>
-			<h5>Attitudes & Behaviors:</h5>
-			<li>She is a social butterfly, very outspoken, stays busy, travel alot (event locations), Advocate for musician and artists,
-				very creative, loves helping people, very optimistic, motivating, go-getter, and passionate.</li>
-			<li>Does not have any form of advertisement for business at the moment but does alot of business on her iPhone X</li>
-			<li>Not only will she be using the website to advertise her work but to also help show my web developing skills.</li>
-		</div>
+			<h2>Purpose,Audience, Goal</h2>
+		<h3>Purpose:</h3>
+			The purpose is to have a home site that advertises business.
+		<h4>Audience:</h4>
+			Targeted to people who are in need of help planning events, social gatherings, and concerts, etc.
+		<h5>Goal:</h5>
+			The goal is to give insight on what Diane does for business and will include contact info to help gain more clients.
+
+		<img id="irene" src="irene.jpg">
+
+
+			<h6>Persona</h6>
+		<div>Name: Diane Dubray</div>
+		<div>Age: 42</div>
+			<div>She is an Oglala Lakota Native from the Pine Ridge Reservation, mother of three, and she will make your event graceful as well as put the effort in which
+				she gives her famiy in all that she does.</div>
+			<div>She is a social butterfly, very outspoken, stays busy, travel alot (event locations), Advocate for musician and artists,
+				very creative, loves helping people, very optimistic, motivating, go-getter, and passionate.</div>
+		<div>She coordinates events such as rallies, banquets, social gatherings, concerts, and etc.</div>
+
+		<b>Likes</b>
+		<ul>
+		<li>Expressing ideas that are out of the ordinary</li>
+		<li>Practicing meditation and being outdoors as a source to stay grounded</li>
+		<li>Staying up to date on social media</li>
+		</ul>
+			<b>Dislikes</b>
+		<ul>
+		<li>Close minded people, small talk</li>
+		<li>Negative energies</li>
+		<li>Being held back</li>
+		</ul>
+		<b>Frustrations</b>
+		Not enough clients as would like to have, want to create website in hopes to get more who are interested in working together as well as finding possible collaborators.
+			Also, would like a purpose for the website to show off my web developing skills.
 	</body>
 </html>
