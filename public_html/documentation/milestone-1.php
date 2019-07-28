a0d525509a3e750d86e3fb844cac774498e6c96c
Lariah Christensen <lariah_14@hotmail.com>
2019-07-28 17:54:49 -0600

    updated persona, rough draft

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -19,11 +19,15 @@
 		<img id="irene" src="irene.jpg">
 
 		<h3>Persona:</h3>
-
+		Name: Rex Manning
+		Age: 31
+		Work: Guitarist/Singer
+		Income: 30K
+		Relationship: Open
 
 		<b>Likes</b>
 		<ul>
-		<li>Inventing new arts</li>
+		<li>Trendy clothing, laid back style.</li>
 		<li>Traveling to new places</li>
 		<li>Knowing the best places to eat in town</li>
 			<li>Asking for opinions</li>
@@ -34,8 +38,32 @@
 			<b>Dislikes</b>
 		<ul>
 		<li>Not knowing where to go next</li>
+			<li>Not knowing about certain events/gigs to play at.</li>
 		<li>Being unorganized</li>
 		<li>Not having enough time to schedule anything.</li>
 		</ul>
+		<h4>Tech:</h4>
+		<ul>
+			<li>Has a samsung s8</li>
+			<li>Has spotify, soundlcoud, and youtube as main music apps.</li>
+			<li>Carries a semi old canon camera</li>
+			<li>Voice Recorder</li>
+			<li>Bluetooth headset, don't really use.</li>
+			<li>Tuner</li>
+		</ul>
+
+		<h5>Attitudes & Behaviors:</h5>
+		<p>Rex is a pretty chill dude, likes going with the flow but also has a decent routine and schedule he lives by. Likes to play around with sound on his guitar during
+		his free time, likes to hit up social events to meet other artists and agents. Loves adventuring and sightseeing, Always asks for others input. Very trendy but also very laid back..
+		Has moments of being a dive but more doesn't care about much except his music.</p>
+		<h5>Frustrations & Needs</h5>
+		<p>Rex needs an event coordinator for his new album coming up, would like to set up a social event so that
+		other artists as well as agents can come along as well, maybe to collaborate. Not exactly sure how to schedule
+		or set up an event with a budget. Looking for a go-getter who gets a sense of his needs and wants to make the environemnt his style.</p>
+		<h6>Goal:</h6>
+		<p>Rex's goal is to make sure that all the his friends as well as their families get together as well as other, mutual friends,
+			and editors will be invited. Everything goes planned accordinaly, sense of style and is organized well.</p>
+		<h7>User Story:</h7>
+		<p>As a user wants to be able to see that this is the perfect coordinator for his party.</p>
 	</body>
 </html>
